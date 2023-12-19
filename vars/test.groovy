fa31ec14268cabdc8f39c3ad3d9f19e07d4fa90f
chilupoorimadhav <madhav9c@gmail.com>
2023-12-19 21:20:52 +0530

    m

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,7 +1,13 @@
-def abc = "Hello"
-def xyz = 10
+//def abc = "Hello"
+//def xyz = 10
 
-print "abc = ${abc}"
-print "xyz = ${xyz}"
+//print "abc = ${abc}"
+//print "xyz = ${xyz}"
 
-print abc
+//print abc
+def new1() {
+    print "hello" 
+    def xyz = "hello"
+    print "xyz" = ${xyz}
+    print "abc" = ${abc}
+}
